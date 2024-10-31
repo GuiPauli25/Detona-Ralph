8f5d84da6ad8072abe4cfc7624d12a2e1182b72b
Guilherme <guilhermeapauli@gmail.com>
2024-10-31 13:57:24 -0300

    Finalização do projeto, complementando o arquivo JS e adicionando a estilização do cursor em CSS.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,12 +1,38 @@
 const state = {
     view: {
-        squares: documents.querySelectorAll(".square"),
-        enemy: documents.querySelector(".enemy"),
-        timeLeft: documents.querySelector("#time-left"),
-        score: documents.querySelector("#score"),
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
     },
-    values: {};
-    };
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 30,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 400),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`)
+    audio.volume = 0.1;
+    audio.play();
+}
 
 function randomSquare() {
     state.view.squares.forEach((square) => {
@@ -16,15 +42,24 @@ function randomSquare() {
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function addListenerHitBox() {
-    state.view.squares.forEach((square) => {});
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
 }
 
 function init() {
-    alert(ola);
-    randomSquare();
+    addListenerHitBox();
 }
 
 init();
